7ce53948a6b4bb77d474a5c41ba22fce6ab31a31
Roy Scheffers <r.scheffers@gmail.com>
2017-02-23 23:54:29 -0500

    Add age and yearsActive range queries

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -6,4 +6,16 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+  return Promise.all([
+    Artist.find({ })
+      .sort({ age: 1 })
+      .limit( 1 ),
+      Artist.find({ })
+        .sort({ age: -1 })
+        .limit( 1 )
+  ]).then( (result) => {
+    let min = result[0][0].age;
+    let max = result[1][0].age;
+    return { min, max }
+  });
 };
--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -6,4 +6,16 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+  return Promise.all([
+    Artist.find({ })
+      .sort({ yearsActive: 1 })
+      .limit( 1 ),
+      Artist.find({ })
+        .sort({ yearsActive: -1 })
+        .limit( 1 )
+  ]).then( (result) => {
+    let min = result[0][0].yearsActive;
+    let max = result[1][0].yearsActive;
+    return { min, max }
+  });
 };
