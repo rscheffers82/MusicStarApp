19db10e6e8ea8713977c1d9d9a8964c74ef6377e
Roy Scheffers <r.scheffers@gmail.com>
2017-02-24 00:43:02 -0500

    Add initial search artists query

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -9,4 +9,23 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  return Promise.all([
+    Artist.find({})
+      .sort({ [sortProperty]: 1 })
+      .skip( offset )
+      .limit( limit ),
+    Artist.count()
+  ])
+  .then( (results) => {
+    let artists = results[0];
+    let count = results[1];
+    console.log('artists: ', artists);
+    console.log('count: ', count);
+    return {
+      all: artists,
+      count: artists.length,
+      offset,
+      limit
+    }
+  });
 };
